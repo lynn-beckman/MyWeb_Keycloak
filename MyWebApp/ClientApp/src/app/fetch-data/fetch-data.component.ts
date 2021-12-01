c700531d936af6a21eec5a7c7a8d756cdc66c7f0
Lynn Fu <LFU01@beckman.com>
2021-12-01 08:50:51 +0800

    Add yarp

--- MyWebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ MyWebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,7 +9,7 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>('https://localhost:7102/weatherforecast').subscribe(result => {
+    http.get<WeatherForecast[]>('https://localhost:7235/weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
