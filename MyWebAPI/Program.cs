f8d8caef0175c3cd711d475e38fd06194f50e562
Lynn Fu <LFU01@beckman.com>
2021-12-04 15:46:46 +0800

    Add Swagger

--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Security.Claims;
 using System.Text.Json;
 
@@ -31,7 +32,44 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Register the Swagger generator, defining 1 or more Swagger documents
+builder.Services.AddSwaggerGen(c =>
+{
+    var scopes = builder.Configuration["Swagger:Scopes"].Split(",", StringSplitOptions.RemoveEmptyEntries);
+    var scopesDic = scopes.ToDictionary(scope => scope, _ => string.Empty);
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = $"Weather API", Version = "v1" });
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows()
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(builder.Configuration["Swagger:AuthorizationUrl"], UriKind.Absolute),
+                TokenUrl = new Uri(builder.Configuration.GetValue<string>("Swagger:TokenUrl")),
+                Scopes = scopesDic
+            }
+        }
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                },
+                Scheme = "oauth2",
+                Name = "oauth2",
+                In = ParameterLocation.Header
+            },
+            scopes.ToArray()
+        }
+    });
+});
 
 var resourceRoleClaimName = $"role/resource/{builder.Configuration["Jwt:Audience"]}";
 builder.Services.AddAuthentication(options =>
@@ -52,8 +90,14 @@ builder.Services.AddAuthentication(options =>
         // Keycloak默认会把First Name和Last Name组合成Name放到Name Claim中, 如果要使用Username, 可以添加下面的代码.
         NameClaimType = "preferred_username"
     };
-    o.Events = new JwtBearerEvents {
-        OnTokenValidated = context => {
+    o.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
             var identity = context.Principal?.Identity as ClaimsIdentity;
             var jsonDocumentOptions = new JsonDocumentOptions
             {
@@ -77,14 +121,14 @@ builder.Services.AddAuthentication(options =>
             if (resourceAccess?.Value != null)
             {
                 using JsonDocument document = JsonDocument.Parse(resourceAccess.Value, jsonDocumentOptions);
-                if( document.RootElement.TryGetProperty("webapi", out var elementWebAPI))
+                if (document.RootElement.TryGetProperty("webapi", out var elementWebAPI))
                 {
-                    if(elementWebAPI.TryGetProperty("roles", out var elementRoles))
+                    if (elementWebAPI.TryGetProperty("roles", out var elementRoles))
                     {
                         foreach (var elementRole in elementRoles.EnumerateArray())
                         {
-                           var role = elementRole.GetString();
-                            if(role != null) identity?.AddClaim(new Claim(resourceRoleClaimName, role));
+                            var role = elementRole.GetString();
+                            if (role != null) identity?.AddClaim(new Claim(resourceRoleClaimName, role));
                         }
                     }
                 }
@@ -100,13 +144,22 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("admins", policy => policy.RequireClaim(resourceRoleClaimName, "admin"));
 });
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint($"/swagger/v1/swagger.json", "Version 1.0");
+        options.OAuthClientId(builder.Configuration["Swagger:ClientId"]);
+        options.OAuthClientSecret(builder.Configuration["Swagger:ClientIdSecret"]);
+        options.OAuthAppName("Weather API");
+        options.OAuthScopeSeparator(" ");
+        options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+    });
 }
 
 app.UseHttpsRedirection();
